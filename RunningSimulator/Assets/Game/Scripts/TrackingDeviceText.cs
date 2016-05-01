f29b7b71138d2b9147cd80a1cbe0cfa4332cbeff
Michael Melatti <melatti.michael@gmail.com>
2016-05-01 00:55:00 -0400

    Fixed Tracking Device Scene
    Made this scene a lot prettier.

---
+++ RunningSimulator/Assets/Game/Scripts/TrackingDeviceText.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TrackingDeviceText : MonoBehaviour {
+
+	public Text myText;
+
+	void Start () {
+		StartCoroutine(Loading());
+	}
+
+	IEnumerator Loading() {
+		while (true) {
+			yield return new WaitForSeconds(2f);
+			myText.text = "Tracking Device";
+			yield return new WaitForSeconds(2f);
+			myText.text = "Tracking Device.";
+			yield return new WaitForSeconds(2f);
+			myText.text = "Tracking Device..";
+			yield return new WaitForSeconds(2f);
+			myText.text = "Tracking Device...";
+		}
+	}
+}
