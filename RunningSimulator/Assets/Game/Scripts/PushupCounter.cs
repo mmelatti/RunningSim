4802c1ec484a4f745b12a3b548e622b3c246def6
Michael Melatti <melatti.michael@gmail.com>
2016-05-01 02:29:11 -0400

    finished Pushup Scene
    Basic scene finished.

---
+++ RunningSimulator/Assets/Game/Scripts/PushupCounter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PushupCounter : MonoBehaviour {
+
+	public Text myText;
+	private bool down = false;
+	public GameObject head;
+
+	void Start () {
+		down = false;
+		PlayerPrefs.SetInt ("Pushups", 0);
+	}
+	
+	void Update () {
+		Debug.Log (head.gameObject.transform.position.y);
+
+		myText.text = PlayerPrefs.GetInt ("Pushups").ToString();
+
+		if (head.gameObject.transform.position.y < .027f) {
+			down = true;
+		}
+		if (head.gameObject.transform.position.y > .15f && down) {
+			down = false;
+			PlayerPrefs.SetInt ("Pushups", PlayerPrefs.GetInt ("Pushups") + 1);
+			Debug.Log (PlayerPrefs.GetInt ("Pushups"));
+		}
+	}
+}
