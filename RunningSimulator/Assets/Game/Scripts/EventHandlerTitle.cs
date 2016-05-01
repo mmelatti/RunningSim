0e5cde28b6be5681f5c2579746ed0cb858aa3a29
Michael Melatti <melatti.michael@gmail.com>
2016-05-01 00:48:24 -0400

    Just added pushup scene,
    I had to restart all of my work from Saturday.

--- RunningSimulator/Assets/Game/Scripts/EventHandlerTitle.cs
+++ RunningSimulator/Assets/Game/Scripts/EventHandlerTitle.cs
@@ -2,14 +2,26 @@
 using System.Collections;
 
 public class EventHandlerTitle : MonoBehaviour {
-
-	void Start () {
-
-	}
 	
 	void Update () {
-	
-	}
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			PlayerPrefs.SetString ("Camera", "cardboard");
+			Application.LoadLevel ("HeadSet");
+		} else if (Input.GetKeyDown (KeyCode.W)) {
+			Application.LoadLevel ("TrackingDevice");
+		} else if (Input.GetKeyDown (KeyCode.E)) {
+			PlayerPrefs.SetString ("Camera", "mainCamera");
+			Application.LoadLevel ("HeadSet");
+		} else if (Input.GetKeyDown (KeyCode.R)) {
+			Application.LoadLevel ("Stats");
+		} else if (Input.GetKeyDown (KeyCode.T)) {
+			PlayerPrefs.SetString ("Camera", "cardboard");
+			Application.LoadLevel ("Pushups");
+		} else if (Input.GetKeyDown (KeyCode.Y)) {
+			PlayerPrefs.SetString ("Camera", "mainCamera");
+			Application.LoadLevel ("Pushups");
+		}
+	} //end update
 
 	public void ButtonStartHeadSet(){
 		PlayerPrefs.SetString ("Camera", "cardboard");
@@ -33,5 +45,13 @@ public class EventHandlerTitle : MonoBehaviour {
 		Application.LoadLevel ("Title");
 	}
 
+	public void ButtonToPushups(){
+		PlayerPrefs.SetString ("Camera", "cardboard");
+		Application.LoadLevel ("Pushups");
+	}
 
+	public void ButtonToPushupsSpectator(){
+		PlayerPrefs.SetString ("Camera", "mainCamera");
+		Application.LoadLevel ("Pushups");
+	}
 }
