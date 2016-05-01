d6a513f490161709931ad5c3e19acc6b0e1b3ff5
Michael Melatti <melatti.michael@gmail.com>
2016-05-01 01:11:19 -0400

    Touch To Title
    This script is included in every scene to help navigate the user back
    to the title screen, whether they are one the PC or the Android device.

---
+++ RunningSimulator/Assets/Game/Scripts/TitleWithTouch.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleWithTouch : MonoBehaviour {
+
+	private bool canStartCoroutine = false;
+	private bool canExit = true;
+
+	void Start () {
+		canStartCoroutine = false;
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.O)) {
+			StartCoroutine (DoSwitchLevel ());
+
+		}
+		if (Input.touchCount >= 3 && canStartCoroutine) {
+			canStartCoroutine = false;
+			StartCoroutine (timer ());
+		}
+	}
+
+	IEnumerator timer(){
+		canExit = true;
+		for (int i = 0; i < 500; i++) {
+			yield return new WaitForSeconds(.01f);
+			if(Input.touchCount < 3){
+				canExit = false;
+				break;
+			}
+		}
+		if (canExit) {
+			StartCoroutine (DoSwitchLevel ());
+		}
+		canStartCoroutine = true;
+	}
+
+	IEnumerator DoSwitchLevel ()
+	{
+		PhotonNetwork.Disconnect ();
+		while (PhotonNetwork.connected)
+			yield return null;
+		Application.LoadLevel("Title");
+	}
+}
