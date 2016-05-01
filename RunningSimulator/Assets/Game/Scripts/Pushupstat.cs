f11da4a4988db2246670063382d9d84f00176435
Michael Melatti <melatti.michael@gmail.com>
2016-05-01 13:58:08 -0400

    fixed stat scene
    Now it displays pushup counter

---
+++ RunningSimulator/Assets/Game/Scripts/Pushupstat.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Pushupstat : MonoBehaviour {
+
+	public Text myText;
+
+	void Start () {
+		myText.text = "Pushups: " + PlayerPrefs.GetInt ("Pushups"); 
+	}
+}
